48beac25663f75c85293edc0596317dcc1272bee
Aria Vesta <dev@ariavesta.com>
2024-10-15 02:55:00 -0600

    Style round button
    Signed-off-by: Aria Vesta <dev@ariavesta.com>

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode } from "react";
 import { Header } from "../components/Header";
 
 import classes from './Layout.module.css'
+import Button from "../components/Button";
 
 export const Layout: React.FC<{ children: ReactNode }> = (props) => (
   <div>
--- src/pages/CreateSecret.tsx
+++ src/pages/CreateSecret.tsx
@@ -1,10 +1,13 @@
 import { CreateSecretForm } from "../components/CreateSecretForm";
 import { Layout } from "../layouts/Layout";
+import Button from "../components/Button";
+import React from "react";
 
 export const CreateSecret = () => {
   return (
     <Layout>
       <h1>Create a Secret</h1>
+      <Button appearance={Button.appearances.round}>🔒 New Secret</Button>
       <CreateSecretForm />
     </Layout>
   );
