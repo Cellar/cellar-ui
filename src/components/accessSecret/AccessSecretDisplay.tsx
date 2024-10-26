0200701b627be17930e887569adf6defb09b6b57
Aria Vesta <dev@ariavesta.com>
2024-10-26 01:47:06 -0600

    Format the access page
    Signed-off-by: Aria Vesta <dev@ariavesta.com>

--- src/components/accessSecret/AccessSecretDisplay.tsx
+++ src/components/accessSecret/AccessSecretDisplay.tsx
@@ -3,9 +3,11 @@ import {Form, TextArea} from '../Form'
 import {useLoaderData} from "react-router-dom";
 import {ISecret} from "../../models/secret";
 import classes from "./AccessSecretDisplay.module.css";
+import {useMediaQuery} from "@mantine/hooks";
 
 export const AccessSecretDisplay = () => {
   const { content: secretContent } = useLoaderData() as ISecret
+  const isMobile = useMediaQuery('(max-width: 1000px)');
 
   async function handleCopySecret() {
     await navigator.clipboard.writeText(secretContent)
@@ -15,9 +17,10 @@ export const AccessSecretDisplay = () => {
     <div>
       <Form>
         <TextArea
+          className={classes.secretContent}
           readOnly={true}
           value={secretContent}
-          rows={8}
+          rows={isMobile ? 11 : 11}
           required/>
         <div className={classes.formSection}>
           <Button appearance={Button.appearances.primary} data-text="Copy Secret" onClick={handleCopySecret}>
