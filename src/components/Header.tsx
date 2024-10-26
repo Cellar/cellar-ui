5deaf1e39a2d186500bbb107c060ba7463135f58
Aria Vesta <dev@ariavesta.com>
2024-10-26 05:24:59 -0600

    Format banner for tiny screen
    Signed-off-by: Aria Vesta <dev@ariavesta.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
-import Button from "./Button";
 import { Logo } from "./Logo";
 
 import classes from './Header.module.css'
+import {useMediaQuery} from "@mantine/hooks";
 
 export function Header() {
+  const isTinyMobile = useMediaQuery('(max-width: 400px)');
+
   return (
     <header>
       <div className={classes.banner}>
-        <p>Share sensitive information securely with cellar. End to end encryption, always free. No sign-up required.</p>
+        {
+          isTinyMobile ? (
+            <p>Share sensitive information securely with cellar.<br />End to end encryption, always free. No sign-up required.</p>
+          ) : (
+            <p>Share sensitive information securely with cellar. End to end encryption, always free. No sign-up required.</p>
+          )
+        }
       </div>
 
       <div className={classes.title}>
