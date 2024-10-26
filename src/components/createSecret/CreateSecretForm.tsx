6c44a7dae5a6eec35c5ff6b354fc2d848edeca24
Aria Vesta <dev@ariavesta.com>
2024-10-26 01:08:10 -0600

    Format create page for mobile
    Signed-off-by: Aria Vesta <dev@ariavesta.com>

--- src/components/createSecret/CreateSecretForm.tsx
+++ src/components/createSecret/CreateSecretForm.tsx
@@ -10,6 +10,7 @@ import {createSecret} from "../../api/client";
 import {ISecretMetadata} from "../../models/secretMetadata";
 import {useNavigate} from "react-router-dom";
 import cx from "classnames";
+import {useMediaQuery} from "@mantine/hooks";
 
 const ExpirationModes = {
   Absolute: 'Expire On (Absolute)',
@@ -23,6 +24,9 @@ export const CreateSecretForm = () => {
   const [accessLimitDisabled, setAccessLimitDisabled] = useState(false)
 
   const navigate = useNavigate()
+
+  const isMobile = useMediaQuery('(max-width: 1000px)');
+
 
   const now = new Date()
   let inTwentyFourHours = new Date()
@@ -43,7 +47,7 @@ export const CreateSecretForm = () => {
     <div>
       <Form>
         <TextArea
-          rows={12}
+          rows={isMobile ? 10 : 12}
           placeholder="Enter Secret Content"
           onChange={e => setSecretContent(e.target.value)}
           required/>
@@ -71,7 +75,6 @@ export const CreateSecretForm = () => {
               }
             </div>
           </div>
-          <div className={classes.shim}/>
           <div className={classes.accessLimitSection}>
             <span className={classes.header}>Access Limit</span>
             <div className={classes.accessLimitElements}>
@@ -84,6 +87,7 @@ export const CreateSecretForm = () => {
               <FormButton className={classes.accessLimitInputModifier} onClick={() => handleSetAccessLimit(accessLimit + 1)}>+</FormButton>
               <p className={classes.orText}>or</p>
               <ToggleButton className={classes.noLimitInput} setParentState={setAccessLimitDisabled}>No Limit</ToggleButton>
+              <div className={classes.shim} />
             </div>
           </div>
         </div>
