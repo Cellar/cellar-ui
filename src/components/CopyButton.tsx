6c588c0af737088d72edc162d171a4a239f4f3a7
Aria Vesta <dev@ariavesta.com>
2025-02-13 23:20:53 -0700

    Automatically adjust size of copy button based on text
    Signed-off-by: Aria Vesta <dev@ariavesta.com>

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import { Checkmark } from "src/components/characters/Checkmark";
 import { Button, ButtonProps } from "src/components/Button";
 import classes from "src/components/CopyButton.module.css";
@@ -14,37 +14,101 @@ export const CopyButton: FC<CopyButtonProps> = ({
   appearance,
   text = "Copy",
   confirmationText = "Copied",
+  id,
   ...props
 }) => {
   const [displayText, setDisplayText] = useState(text);
+  const contentRef = useRef<HTMLSpanElement>(null);
+  const [contentWidth, setContentWidth] = useState<number | null>(null);
+
+  const checkMarkId = `${id}-checkmark`;
+
+  function getCheckMarkWidth(): number {
+    const checkMark = document.getElementById(checkMarkId);
+    if (checkMark) {
+      const checkMarkStyles = window.getComputedStyle(checkMark);
+      return +checkMarkStyles.width.split("px")[0];
+    }
+
+    return 0;
+  }
+
+  function getMaxContentWidth(): number {
+    if (contentRef.current) {
+      const textSpan = document.createElement("span");
+      const confirmationSpan = document.createElement("span");
+
+      const styles = window.getComputedStyle(contentRef.current);
+      [textSpan, confirmationSpan].forEach((span) => {
+        span.style.visibility = "hidden";
+        span.style.position = "absolute";
+        span.style.font = styles.font;
+        span.style.padding = styles.padding;
+        document.body.appendChild(span);
+      });
+
+      textSpan.textContent = text;
+      confirmationSpan.textContent = confirmationText;
+
+      const maxWidth = Math.max(
+        textSpan.offsetWidth,
+        confirmationSpan.offsetWidth,
+      );
+
+      textSpan.remove();
+      confirmationSpan.remove();
+
+      return maxWidth;
+    }
+
+    return 0;
+  }
+
+  function getGapWidth(): number {
+    if (contentRef.current)
+      return parseInt(getComputedStyle(contentRef.current).gap) || 8;
+
+    return 8;
+  }
+
+  useEffect(() => {
+    setContentWidth(getMaxContentWidth() + getGapWidth() + getCheckMarkWidth());
+  }, [text, confirmationText]);
 
   const handleClick = async () => {
     await navigator.clipboard.writeText(textToCopy);
     setDisplayText(confirmationText);
     setTimeout(() => {
       setDisplayText(text);
-    }, 10000);
+    }, 5000);
   };
 
   return (
     <Button appearance={appearance} {...props} onClick={handleClick}>
-      {displayText}{" "}
-      {displayText === confirmationText && (
-        <Checkmark
-          className={(() => {
-            switch (appearance) {
-              case Button.appearances.primary:
-                return classes.checkPrimary;
-              case Button.appearances.secondary:
-                return classes.checkSecondary;
-              case Button.appearances.round:
-                return classes.checkRound;
-              default:
-                return "";
-            }
-          })()}
-        />
-      )}
+      <span
+        ref={contentRef}
+        className={classes.buttonContent}
+        style={contentWidth ? { width: `${contentWidth}px` } : undefined}
+      >
+        {displayText}
+        {displayText === confirmationText && (
+          <Checkmark
+            id={checkMarkId}
+            className={(() => {
+              switch (appearance) {
+                case Button.appearances.primary:
+                  return classes.checkPrimary;
+                case Button.appearances.secondary:
+                  return classes.checkSecondary;
+                case Button.appearances.round:
+                  return classes.checkRound;
+                default:
+                  return "";
+              }
+            })()}
+          />
+        )}
+      </span>
     </Button>
   );
 };
--- src/components/characters/Checkmark.tsx
+++ src/components/characters/Checkmark.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from "react";
 
-export const Checkmark: FC<{ className?: string }> = ({
+export const Checkmark: FC<{ className?: string; id?: string }> = ({
   className,
+  id,
   ...props
 }) => (
   <svg
+    id={id}
     className={className}
     viewBox="0 0 11 9"
     fill="none"
