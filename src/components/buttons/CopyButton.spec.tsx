54a893ee70955b355623af97a74eb76076929d6a
Aria Vesta <dev@ariavesta.com>
2025-02-16 00:10:17 -0700

    Update naming of copy button tests
    Signed-off-by: Aria Vesta <dev@ariavesta.com>

--- src/components/buttons/CopyButton.spec.tsx
+++ src/components/buttons/CopyButton.spec.tsx
@@ -8,7 +8,7 @@ import {
 } from "@testing-library/react";
 import { CopyButton } from "./CopyButton";
 
-describe("CopyButton", () => {
+describe("When rendering CopyButton", () => {
   const mockWriteText = vi.fn();
 
   beforeEach(() => {
@@ -20,93 +20,107 @@ describe("CopyButton", () => {
     });
   });
 
-  it("renders the button with the correct initial text", () => {
-    render(<CopyButton textToCopy="Hello, World!" text="Copy" />);
-    expect(screen.getByText("Copy")).toBeInTheDocument();
-  });
-
-  it("copies the text to the clipboard when clicked", async () => {
-    render(<CopyButton textToCopy="Hello, World!" text="Copy" />);
-    fireEvent.click(screen.getByText("Copy"));
-    await waitFor(() =>
-      expect(mockWriteText).toHaveBeenCalledWith("Hello, World!"),
-    );
-  });
-
-  it("displays the confirmation text after copying", async () => {
-    render(
-      <CopyButton
-        textToCopy="Hello, World!"
-        text="Copy"
-        confirmationText="Copied"
-      />,
-    );
-    fireEvent.click(screen.getByText("Copy"));
-    await waitFor(() => expect(screen.getByText("Copied")).toBeInTheDocument());
-  });
-
-  it("resets the display text back to the original after 3 seconds", async () => {
-    vi.useFakeTimers();
-
-    const clipboardWriteMock = vi.fn(() => Promise.resolve());
-    Object.assign(navigator, {
-      clipboard: {
-        writeText: clipboardWriteMock,
-      },
+  describe("and initially displaying the button", () => {
+    it("should show the correct initial text", () => {
+      render(<CopyButton textToCopy="Hello, World!" text="Copy" />);
+      expect(screen.getByText("Copy")).toBeInTheDocument();
     });
-    render(
-      <CopyButton
-        textToCopy="Hello, World!"
-        text="Copy"
-        confirmationText="Copied"
-      />,
-    );
+  });
 
-    await act(async () => {
+  describe("and clicking the button", () => {
+    it("should copy the text to the clipboard", async () => {
+      render(<CopyButton textToCopy="Hello, World!" text="Copy" />);
       fireEvent.click(screen.getByText("Copy"));
-      await clipboardWriteMock();
+      await waitFor(() =>
+        expect(mockWriteText).toHaveBeenCalledWith("Hello, World!"),
+      );
     });
 
-    expect(screen.getByText("Copied")).toBeInTheDocument();
-    await act(async () => {
-      await vi.advanceTimersByTimeAsync(3000);
+    it("should display the confirmation text", async () => {
+      render(
+        <CopyButton
+          textToCopy="Hello, World!"
+          text="Copy"
+          confirmationText="Copied"
+        />,
+      );
+      fireEvent.click(screen.getByText("Copy"));
+      await waitFor(() =>
+        expect(screen.getByText("Copied")).toBeInTheDocument(),
+      );
     });
 
-    expect(screen.getByText("Copy")).toBeInTheDocument();
+    describe("and waiting for 3 seconds", () => {
+      it("should reset the display text back to original", async () => {
+        vi.useFakeTimers();
 
-    vi.useRealTimers();
+        const clipboardWriteMock = vi.fn(() => Promise.resolve());
+        Object.assign(navigator, {
+          clipboard: {
+            writeText: clipboardWriteMock,
+          },
+        });
+        render(
+          <CopyButton
+            textToCopy="Hello, World!"
+            text="Copy"
+            confirmationText="Copied"
+          />,
+        );
+
+        await act(async () => {
+          fireEvent.click(screen.getByText("Copy"));
+          await clipboardWriteMock();
+        });
+
+        expect(screen.getByText("Copied")).toBeInTheDocument();
+        await act(async () => {
+          await vi.advanceTimersByTimeAsync(3000);
+        });
+
+        expect(screen.getByText("Copy")).toBeInTheDocument();
+
+        vi.useRealTimers();
+      });
+    });
   });
 
-  it("does not display the checkmark if 'showCheckmark' is false", () => {
-    render(
-      <CopyButton
-        textToCopy="Hello, World!"
-        text="Copy"
-        showCheckmark={false}
-      />,
-    );
-    fireEvent.click(screen.getByText("Copy"));
-    expect(screen.queryByTestId(/checkmark/i)).not.toBeInTheDocument();
-  });
+  describe("and configuring checkmark visibility", () => {
+    describe("and showCheckmark is false", () => {
+      it("should not display the checkmark", () => {
+        render(
+          <CopyButton
+            textToCopy="Hello, World!"
+            text="Copy"
+            showCheckmark={false}
+          />,
+        );
+        fireEvent.click(screen.getByText("Copy"));
+        expect(screen.queryByTestId(/checkmark/i)).not.toBeInTheDocument();
+      });
+    });
 
-  it("displays the checkmark when 'showCheckmark' is true and confirmation text is shown", async () => {
-    render(
-      <CopyButton
-        id="test-button"
-        textToCopy="Hello, World!"
-        text="Copy"
-        confirmationText="Copied"
-        showCheckmark={true}
-      />,
-    );
+    describe("and showCheckmark is true", () => {
+      it("should display the checkmark when confirmation text is shown", async () => {
+        render(
+          <CopyButton
+            id="test-button"
+            textToCopy="Hello, World!"
+            text="Copy"
+            confirmationText="Copied"
+            showCheckmark={true}
+          />,
+        );
 
-    fireEvent.click(screen.getByText("Copy"));
+        fireEvent.click(screen.getByText("Copy"));
 
-    await waitFor(() => {
-      expect(screen.getByText("Copied")).toBeInTheDocument();
-      expect(
-        document.getElementById("test-button-checkmark"),
-      ).toBeInTheDocument();
+        await waitFor(() => {
+          expect(screen.getByText("Copied")).toBeInTheDocument();
+          expect(
+            document.getElementById("test-button-checkmark"),
+          ).toBeInTheDocument();
+        });
+      });
     });
   });
 });
