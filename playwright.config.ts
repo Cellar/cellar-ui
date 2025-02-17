5343af46b898499d7fce99467a0850f28c32c09f
Aria Vesta <dev@ariavesta.com>
2025-02-16 17:44:20 -0700

    Add test reporter for junit and setup in gitlab
    Signed-off-by: Aria Vesta <dev@ariavesta.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFile: 'test-results/html' }],
+    ['junit', { outputFile: 'test-results/junit.xml' }],
+  ],
   use: {
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
