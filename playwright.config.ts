0af2d96f98f124b0e760448114a9b0a40dc22b94
Aria Vesta <dev@ariavesta.com>
2025-02-19 02:43:03 -0700

    Split e2e tests into multiple junit reporters
    Signed-off-by: Aria Vesta <dev@ariavesta.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,99 @@
 import { defineConfig, devices } from '@playwright/test';
 
+function getReporter(browserName: string): [string, object] {
+  return [
+    'junit',
+    {
+      outputFile: `./test-results/e2e/junit-${browserName}.xml`,
+      testSuiteTitle: `${browserName} Tests`,
+      suiteNameFormat: `[${browserName}] {suite}`,
+      testCaseNameFormat: `[${browserName}] {title}`,
+    },
+  ];
+}
+
+const projects = [
+  {
+    name: 'chromium-desktop',
+    use: { ...devices['Desktop Chrome'] },
+    reporter: getReporter('chromium-desktop'),
+  },
+  {
+    name: 'firefox-desktop',
+    use: { ...devices['Desktop Firefox'] },
+    reporter: getReporter('firefox-desktop'),
+  },
+  {
+    name: 'webkit-desktop',
+    use: { ...devices['Desktop Safari'] },
+    reporter: getReporter('webkit-desktop'),
+  },
+  {
+    name: 'chromium-mobile',
+    use: { ...devices['Pixel 7'] },
+    reporter: getReporter('chromium-mobile'),
+  },
+  {
+    name: 'chromium-mobile-landscape',
+    use: { ...devices['Pixel 7 landscape'] },
+    reporter: getReporter('chromium-mobile-landscape'),
+  },
+  {
+    name: 'chromium-mobile-old',
+    use: { ...devices['Pixel 3'] },
+    reporter: getReporter('chromium-mobile-old'),
+  },
+  {
+    name: 'chromium-mobile-old-landscape',
+    use: { ...devices['Pixel 3 landscape'] },
+    reporter: getReporter('chromium-mobile-old-landscape'),
+  },
+  {
+    name: 'webkit-mobile',
+    use: { ...devices['iPhone 15 Pro Max'] },
+    reporter: getReporter('webkit-mobile'),
+  },
+  {
+    name: 'webkit-mobile-landscape',
+    use: { ...devices['iPhone 15 Pro Max landscape'] },
+    reporter: getReporter('webkit-mobile-landscape'),
+  },
+  {
+    name: 'webkit-mobile-old',
+    use: { ...devices['iPhone X'] },
+    reporter: getReporter('webkit-mobile-old'),
+  },
+  {
+    name: 'webkit-mobile-old-landscape',
+    use: { ...devices['iPhone X landscape'] },
+    reporter: getReporter('webkit-mobile-old-landscape'),
+  },
+  {
+    name: 'figma',
+    use: {
+      ...devices['Desktop Chrome'],
+      viewport: { width: 1194, height: 834 },
+    },
+    reporter: getReporter('figma'),
+  },
+  {
+    name: 'figma-mobile',
+    use: {
+      ...devices['Pixel 7'],
+      viewport: { width: 393, height: 852 },
+    },
+    reporter: getReporter('figma-mobile'),
+  },
+  {
+    name: 'figma-mobile-tiny',
+    use: {
+      ...devices['Pixel 7'],
+      viewport: { width: 350, height: 852 },
+    },
+    reporter: getReporter('figma-mobile-tiny'),
+  },
+];
+
 export default defineConfig({
   testDir: './tests/e2e',
   testIgnore: '**/src/**',
@@ -9,65 +103,19 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [
-    ['html', { outputFile: 'test-results/e2e/html' }],
-    ['junit', { outputFile: 'test-results/e2e/junit.xml' }],
-  ],
   use: {
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
+  reporter: [['list'], ...projects.map((project) => project.reporter)],
   projects: [
     {
       name: 'setup',
       testMatch: /.*\.setup\.ts/,
     },
-    { name: 'chromium-desktop', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox-desktop', use: { ...devices['Desktop Firefox'] } },
-    { name: 'webkit-desktop', use: { ...devices['Desktop Safari'] } },
-    { name: 'chromium-mobile', use: { ...devices['Pixel 7'] } },
-    {
-      name: 'chromium-mobile-landscape',
-      use: { ...devices['Pixel 7 landscape'] },
-    },
-    { name: 'chromium-mobile-old', use: { ...devices['Pixel 3'] } },
-    {
-      name: 'chromium-mobile-old-landscape',
-      use: { ...devices['Pixel 3 landscape'] },
-    },
-    { name: 'webkit-mobile', use: { ...devices['iPhone 15 Pro Max'] } },
-    {
-      name: 'webkit-mobile-landscape',
-      use: { ...devices['iPhone 15 Pro Max landscape'] },
-    },
-    { name: 'webkit-mobile-old', use: { ...devices['iPhone X'] } },
-    {
-      name: 'webkit-mobile-old-landscape',
-      use: { ...devices['iPhone X landscape'] },
-    },
-    {
-      name: 'figma',
-      use: {
-        ...devices['Desktop Chrome'],
-        viewport: { width: 1194, height: 834 },
-      },
-    },
-    {
-      name: 'figma-mobile',
-      use: {
-        ...devices['Pixel 7'],
-        viewport: { width: 393, height: 852 },
-      },
-    },
-    {
-      name: 'figma-mobile-tiny',
-      use: {
-        ...devices['Pixel 7'],
-        viewport: { width: 350, height: 852 },
-      },
-    },
+    ...projects,
   ],
   webServer: {
     command: 'npm run dev',
